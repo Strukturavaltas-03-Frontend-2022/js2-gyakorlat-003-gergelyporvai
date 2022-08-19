b43c336ed1fba67278c78883281604101c442da9
GergelyPorvai <porvai.gergely@gmail.com>
2022-08-19 20:33:13 +0200

    mind kész

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,4 +1,15 @@
+/*- Mentsd el külön `l`, `f` és `j` változókba a fenti objektum lastName, firstName és job property-jeit! 
+- Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string! */
+
 const user = {
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: "John",
+  lastName: "Doe",
 };
+
+const {
+  firstName: f = "unknown",
+  lastName: l = "unknown",
+  job: j = "unknown",
+} = user;
+
+export { f, l, j };
